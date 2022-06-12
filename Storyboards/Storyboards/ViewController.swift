793cda22ddd13a409fb3ec345a4dd82f12eb6442
TuanDao2002 <tuan09092002@gmail.com>
2022-06-12 17:03:51 +0700

    add storyboard project

---
+++ Storyboards/Storyboards/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Storyboards
+//
+//  Created by Tuan Dao on 12/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        label.text = "hi everyone!"
+    }
+
+
+}
+
