207db3b0291e8697e6c77c5cd13d712199076db3
TuanDao2002 <tuan09092002@gmail.com>
2022-06-12 12:42:12 +0700

    new project

---
+++ First Project/First Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  First Project
+//
+//  Created by Tuan Dao on 12/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .font(.body)
+            .fontWeight(.bold)
+            .foregroundColor(Color.red)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First Project/First Project/First_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_ProjectApp.swift
+//  First Project
+//
+//  Created by Tuan Dao on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct First_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
