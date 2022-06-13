b59f006bf631dcf619b96062436ccb1cd5766ebf
TuanDao2002 <tuan09092002@gmail.com>
2022-06-13 17:31:43 +0700

    input text field that is focused first after launch and the keyboard disapper when touch elsewhere

--- Storyboards/Storyboards/ViewController.swift
+++ Storyboards/Storyboards/ViewController.swift
@@ -9,13 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        label.text = "hi everyone!"
+        label.text = "Hi Everyone!"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        textField.becomeFirstResponder()
     }
 
-
+    @IBAction func buttonWasPressed(_ sender: Any) {
+        let name = textField.text ?? ""
+        label.text = "Hi \(name)!"
+        textField.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textField.resignFirstResponder()
+    }
 }
 
