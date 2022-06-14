a2d35bd6db12e979b71f6b51bf83e404f7b20c28
TuanDao2002 <tuan09092002@gmail.com>
2022-06-14 11:08:28 +0700

    new project

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Tuan Dao on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
