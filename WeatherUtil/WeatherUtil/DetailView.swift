f5cff4f41cb53c19337e0b07626d28bcd0a81c48
TuanDao2002 <tuan09092002@gmail.com>
2022-06-16 18:39:55 +0700

    create new project

---
+++ WeatherUtil/WeatherUtil/DetailView.swift
@@ -0,0 +1,33 @@
+//
+//  DetailView.swift
+//  WeatherUtil
+//
+//  Created by Tuan Dao on 16/06/2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    var data: WeatherData
+    @State var isPopoverShowing = false
+    
+    var body: some View {
+        VStack {
+            Text(data.day)
+            Button("Show Popover") {
+                isPopoverShowing = true
+            }
+            .padding()
+            .sheet(isPresented: $isPopoverShowing) {
+                Text("H \(data.high)º L \(data.low)º F")
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(data: DataModel.data[0])
+    }
+}
---
+++ WeatherUtil/WeatherUtil/WeatherUtilApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUtilApp.swift
+//  WeatherUtil
+//
+//  Created by Tuan Dao on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUtilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
