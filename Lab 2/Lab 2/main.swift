d7094cf53251d758c83f71f2ddded27d534691ba
TuanDao2002 <tuan09092002@gmail.com>
2022-07-02 11:35:42 +0700

    update

--- Lab 2/Lab 2/main.swift
+++ Lab 2/Lab 2/main.swift
@@ -115,6 +115,7 @@ musk.logStatus()
 */
 
 // ex 5
+/*
 let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 func generatePassword() -> String {
@@ -127,4 +128,162 @@ func generatePassword() -> String {
 
 let password = generatePassword()//Replace this comment with your code.
 print(password)
+*/
+
+// ex 6
+
+/*
+var ticket: [Int] = []
+var winningNumbers: [Int] = []
+
+func generateRandom() -> [Int] {
+    var numArr: [Int] = []
+    for _ in 0..<6 {
+        numArr.append(Int.random(in: 1...49))
+    }
+    return numArr
+}
+
+while true {
+    print("Do you want to pick your own ticket numbers (true/false)?")
+    var pick = false
+    let input = readLine()
+    if (input == "true") {
+        pick = true
+    }
+        
+    if pick {
+        print("Enter 6 numbers of your ticket (1-49):")
+        ticket = readLine()?.components(separatedBy: " ").compactMap({Int($0)}) ?? []
+        var isValid = true
+        if ticket.count < 6 {
+            print("Invalid ticket")
+            isValid = false
+        }
+        
+        for index in 0..<ticket.count {
+            if (ticket[index] < 1 || ticket[index] > 49) {
+                print("All numbers must be between 1 and 49")
+                isValid = false
+            }
+        }
+        
+        if !isValid {
+            continue
+        }
+    } else {
+        ticket = generateRandom()
+    }
+    
+    print("You have picked the ticket: ", terminator: "")
+    ticket.forEach{num in
+        print("\(num)", terminator: " ")
+    }
+    
+    winningNumbers = generateRandom()
+    print("\nThe winning ticket is: ", terminator: "")
+    winningNumbers.forEach{num in
+        print("\(num)", terminator: " ")
+    }
+    print()
+    
+    var match = 0
+    for index in 0..<6 {
+        if (ticket[index] == winningNumbers[index]) {
+            match += 1
+        }
+    }
+    print("Your ticket has matched \(match) number(s)")
+    
+    var prize = 0
+    switch match {
+    case 1, 2:
+        prize = 10
+    case 3:
+        prize = 100
+    case 4:
+        prize = 1000
+    case 5:
+        prize = 5000
+    case 6:
+        prize = 5000000
+    default:
+        prize = 0
+    }
+    print("You have won $\(prize)")
+    
+    print("Do you want to continue(yes/no)?")
+    let isContinue = readLine()
+    if isContinue != "yes" {
+        break
+    }
+}
+*/
+
+// ex6 - bonus
+
+var ticket: [Int] = []
+var winningNumbers: [Int] = []
+var gamesWon = 0
+var gamesMatchAll6Nums = 0
+var totalMoneyWon = 0
+var totalTicketCost = 0
+
+func generateRandom() -> [Int] {
+    var numArr: [Int] = []
+    for _ in 0..<6 {
+        numArr.append(Int.random(in: 1...49))
+    }
+    return numArr
+}
+
+print("How many games do you want to play")
+var numOfGames = Int(readLine()!)!
+if numOfGames > 0 {
+    for _ in 0..<numOfGames {
+        ticket = generateRandom()
+        winningNumbers = generateRandom()
+        
+        var match = 0
+        for index in 0..<6 {
+            if (ticket[index] == winningNumbers[index]) {
+                match += 1
+            }
+        }
+        
+        var prize = 0
+        switch match {
+        case 1, 2:
+            prize = 10
+        case 3:
+            prize = 100
+        case 4:
+            prize = 1000
+        case 5:
+            prize = 5000
+        case 6:
+            prize = 5000000
+        default:
+            prize = 0
+        }
+
+        if prize > 0 {
+            totalMoneyWon += prize
+            gamesWon += 1
+        }
+        
+        if prize == 5000000 {
+            gamesMatchAll6Nums += 1
+        }
+    }
+}
+
+totalTicketCost = numOfGames * 4
+print("The number of games won any money: \(gamesWon)")
+print("The number of games matched all 6 numbers: \(gamesMatchAll6Nums)")
+print("Total money won: $\(totalMoneyWon)")
+print("Total ticket cost: $\(totalTicketCost)")
+print("Profit/Loss (won-cost): $\(totalMoneyWon - totalTicketCost)")
+
+
 
