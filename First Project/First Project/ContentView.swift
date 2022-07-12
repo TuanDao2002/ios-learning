10e5d27b29f7cd0c6879fba76bcb55953687b667
TuanDao2002 <tuan09092002@gmail.com>
2022-07-12 14:30:17 +0700

    update

--- First Project/First Project/ContentView.swift
+++ First Project/First Project/ContentView.swift
@@ -9,16 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .font(.body)
-            .fontWeight(.bold)
-            .foregroundColor(Color.red)
-            .padding()
+        ZStack{
+            Color(red: 0.03, green: 0.03, blue: 0.35).edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("RMIT-logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
